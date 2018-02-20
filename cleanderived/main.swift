8c386b67a4e5723af6916039b0cda0810afebfd6
Jeffery Kuo <jeffery.kuo@commissionsinc.com>
2018-02-20 11:25:08 -0500

    Initial Commit

---
+++ cleanderived/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  cleanderived
+//
+//  Created by Jeffery Kuo on 2/20/18.
+//  Copyright © 2018 Jeffery Kuo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
