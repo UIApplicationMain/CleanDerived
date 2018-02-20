2b226454b35ae583d476396d1b23e90a52fafedf
Jeffery Kuo <jeffckuo@gmail.com>
2018-02-20 15:43:19 -0500

    Working

--- cleanderived/main.swift
+++ cleanderived/main.swift
@@ -8,5 +8,16 @@
 
 import Foundation
 
-print("Hello, World!")
+let shell = Shell()
 
+let tildeDirectory = NSHomeDirectory()
+
+let derivedData = "\(tildeDirectory)/Library/Developer/Xcode/DerivedData"
+print("Deleting \(derivedData)")
+let _ = shell.bash(command: "rm", arguments: ["-r", "-f", derivedData])
+print(shell.bash(command: "ls", arguments: [derivedData]))
+
+let caches = "\(tildeDirectory)/Library/Caches/com.apple.dt.Xcode"
+print("Deleting \(caches)")
+let _ = shell.bash(command: "rm", arguments: ["-r", "-f", caches])
+print(shell.bash(command: "ls", arguments: [caches]))
